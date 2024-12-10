15d27b110d58c7fd3d51e377d2344a9e99406695
Alexandru Grigore <138763074+Alexandru1654@users.noreply.github.com>
2024-12-10 13:59:17 +0100

    Set up Express and MySQL connection

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
+
+db.connect(err => {
+    if (err) {
+        console.error('Errore di connessione: ' + err.stack);
+        return;
+    }
+    console.log('Connesso al database con ID ' + db.threadId);
+});
+
+app.use(express.json());
+
